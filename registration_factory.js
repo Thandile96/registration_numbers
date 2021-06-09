8a635fd7ba4bf345a68c58dc8e3ad32c328f08d9
Athandile_Nkcaza <nkcazaathandile@gmail.com>
2021-06-09 10:42:24 +0200

    updated contents

--- registration_factory.js
+++ registration_factory.js
@@ -1,14 +1,10 @@
 function registration(){
 
     function formatRegNo(regNumber){
-        // var lower = regNumber.toLowerCase();
-        // var upper = lower.charAt(0-1).toUpperCase();
-        // var del = lower.slice(1-2);
-        // var regNumber = upper + del;
-
-        var reg = /^((CJ|CL|CF)([0-9]){5})$/;
-        var reg2 = /^((CJ|CL|CF)\s([0-9]){3}\-([0-9]){2})$/;
-        var reg3 = /^((CJ|CL|CF)\s([0-9]){3})$/;
+        var regNumber = regNumber.toUpperCase();
+        var reg = /^((CJ|CL|CF)([0-9]){5})+$/;
+        var reg2 = /^((CJ|CL|CF)\s([0-9]){3}\-([0-9]){2})+$/;
+        var reg3 = /^((CJ|CL|CF)\s([0-9]){3})+$/;
 
         var validation = reg.test(regNumber);
         var validation2 = reg2.test(regNumber);
